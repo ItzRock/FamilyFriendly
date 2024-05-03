652b67670084615c46bea1a5ab44fbe18b2b1286
Anthony Stainton <anthonystainton@gmail.com>
2024-05-02 21:47:03 -0700

    finally works

--- FamilyFriendly/Plugin.cs
+++ FamilyFriendly/Plugin.cs
@@ -1,11 +1,14 @@
 ﻿using BepInEx;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using Unity.VisualScripting;
 using VoiceRecognitionAPI;
 namespace FamilyFriendly {
     [ContentWarningPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_VERSION, true)]
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin {
-        readonly string path = $"{Paths.PluginPath}\\naughtywords.txt";
+        readonly string filename = $"naughtywords.txt";
    
         void Kill(string msg) {
             Logger.LogInfo($"Bro said: {msg}");
@@ -14,18 +17,24 @@ namespace FamilyFriendly {
 
         private void Awake() {
             // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
-            Logger.LogInfo($"ASSUMING PATH: {path}");
-            // stolen https://www.c-sharpcorner.com/UploadFile/mahesh/how-to-read-a-text-file-in-C-Sharp/
-            using (StreamReader file = new StreamReader(path)) {
-                int counter = 0;
-                string ln;
-                while ((ln = file.ReadLine()) != null) {
-                    Voice.ListenForPhrase(ln, Kill);
-                    counter++;
+            Logger.LogInfo($"Searching folder: {Paths.PluginPath}");
+            List<string> files = Directory.GetFiles(Paths.PluginPath, filename, SearchOption.AllDirectories).ToList();
+            if(files.Count < 1) { Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} failed to load! Missing {filename}"); return; }
+            for(int i = 0; i < files.Count; i++){
+                string path = files[i];
+                Logger.LogInfo($"LOADING PATH: {path}");
+                // stolen https://www.c-sharpcorner.com/UploadFile/mahesh/how-to-read-a-text-file-in-C-Sharp/
+                using (StreamReader file = new StreamReader(path)) {
+                    int counter = 0;
+                    string ln;
+                    while ((ln = file.ReadLine()) != null) {
+                        Voice.ListenForPhrase(ln, Kill);
+                        counter++;
+                    }
+                    file.Close();
                 }
-                file.Close();
             }
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
     }
 }
